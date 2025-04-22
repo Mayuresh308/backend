7fe384bbc28285cf190fe9b3ffb850bb12b88296
Mayuresh308 <mayureshmayuresh56@gmail.com>
2025-04-23 01:51:59 +0530

    updated

--- server/utils/runPython.js
+++ server/utils/runPython.js
@@ -2,16 +2,25 @@ const { spawn } = require("child_process");
 
 function runPython() {
   return new Promise((resolve, reject) => {
-    const python = spawn("python", ["news_script.py"]);
+    // const python = spawn("python", ["news_script.py"]);
+    const path = require("path");
+    const scriptPath = path.join(__dirname, "..", "..", "news_script.py");
+    const python = spawn("python3", [scriptPath]);
+
     let data = "";
 
     python.stdout.on("data", (chunk) => {
       data += chunk.toString();
     });
 
+    // python.stderr.on("data", (err) => {
+    //   console.error("Python error:", err.toString());
+    // });
+
     python.stderr.on("data", (err) => {
-      console.error("Python error:", err.toString());
+      console.error("Python stderr:", err.toString());
     });
+    
 
     python.on("close", (code) => {
       if (code !== 0) return reject("Python script failed");
